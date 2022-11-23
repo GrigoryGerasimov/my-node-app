f05a01655247d1bfbf3af1ba258f879a42c2060d
Grigory <rehor.ger@gmail.com>
2022-11-23 22:45:14 +0100

    progress backup v3

--- node/modules/commands.js
+++ node/modules/commands.js
@@ -10,7 +10,7 @@ const createNewFile = async (dirPath, fileName, fileText) => {
             await fs.writeFile(newFilePath, JSON.stringify(fileText));
         } else await fs.appendFile(newFilePath, JSON.stringify(fileText));
     } finally {
-        console.log(chalk.magenta("command completed"))
+        console.log(chalk.magenta("command completed"));
     }
 };
 
@@ -19,14 +19,14 @@ const createNewDir = async dirName => {
     try {
         if (!fsSync.existsSync(newDirPath)) {
             await fs.mkdir(newDirPath);
-            await createNewFile(newDirPath, "testFile.txt", "New file in new folder created");
+            await createNewFile(newDirPath, "init.txt", "New folder created");
             return newDirPath;
         } else {
-            await createNewFile(newDirPath, "testFile.txt", "New file in already existing folder created");
+            await createNewFile(newDirPath, "init.txt", "Already existing folder initialized");
             return newDirPath
         };
     } finally {
-        console.log(chalk.magenta("command completed"))
+        console.log(chalk.magenta("command completed"));
     }
 };
 
@@ -37,12 +37,67 @@ const readFromFile = async fileName => {
     } catch (error) {
         console.log(chalk.red(error));
     } finally {
-        console.log(chalk.magenta("command completed"))
+        console.log(chalk.magenta("command completed"));
+    }
+};
+
+const removeFile = async filePath => {
+    try {
+        await fs.rm(filePath, { recursive: true });
+    } catch (error) {
+        console.log(chalk.red(error));
+    } finally {
+        console.log(chalk.magenta("command completed"));
+    }
+};
+
+const removeDir = async dirPath => {
+    try {
+        await fs.rmdir(dirPath, { recursive: true });
+    } catch (error) {
+        console.log(chalk.red(error));
+    } finally {
+        console.log(chalk.magenta("command completed"));
+    }
+};
+
+const truncateFile = async filePath => {
+    try {
+        await fs.truncate(filePath, 0);
+    } catch (error) {
+        console.log(chalk.red(error));
+    } finally {
+        console.log(chalk.magenta("command completed"));
+    }
+}
+
+const copy = async (srcPath, destPath) => {
+    try {
+        await fs.copyFile(srcPath, destPath);
+    } catch (error) {
+        console.log(chalk.red(error));
+    } finally {
+        console.log(chalk.magenta("command completed"));
+    }
+};
+
+const rename = async (oldPath, newPath) => {
+    try {
+        await fs.rename(oldPath, newPath);
+    } catch (error) {
+        console.log(chalk.red(error));
+    } finally {
+        console.log(chalk.magenta("command completed"));
     }
 };
 
 module.exports = {
     createNewDir,
     createNewFile,
-    readFromFile
+    readFromFile,
+    removeFile,
+    removeDir,
+    truncateFile,
+    copy,
+    rename
 };
--- node/modules/fsmodule.js
+++ node/modules/fsmodule.js
@@ -1,10 +1,32 @@
 const yargs = require("yargs");
 const path = require("path");
-const fsSync = require("fs");
-const fs = require("fs/promises");
+const os = require("os");
 const { createNewDir,
     createNewFile,
-    readFromFile } = require("./commands.js");
+    readFromFile,
+    removeFile,
+    removeDir,
+    truncateFile,
+    copy,
+    rename
+} = require("./commands.js");
+
+const osInfo = {
+    platform: os.platform(),
+    freeMemory: os.freemem(),
+    totalMemory: os.totalmem(),
+    cpus: os.cpus(),
+    type: os.type(),
+    constants: os.constants,
+    homedir: os.homedir(),
+    tmpdir: os.tmpdir(),
+    hostname: os.hostname(),
+    networkInterfaces: os.networkInterfaces(),
+    architecture: os.arch(),
+    version: os.version(),
+    userInfo: os.userInfo(),
+    uptime: os.uptime()
+}
 
 yargs.command({
     command: "newdir",
@@ -28,27 +50,130 @@ yargs.command({
             type: "string",
             description: "new file name",
             demandOption: true
+        },
+        newfiletext:{
+            type: "string",
+            description: "new file text",
+            demandOption: true
         }
     },
     description: "new file",
-    async handler({ newfilename }) {
-        await createNewFile(newfilename, process.argv[2]);
+    async handler({ newfilename, newfiletext }) {
+        await createNewFile(__dirname, newfilename, newfiletext);
     }
 })
 
 yargs.command({
     command: "readfile",
     builder: {
-        fileTitle: {
+        filetitle: {
             type: "string",
             description: "test file name",
             demandOption: true
         }
     },
     description: "read from file",
-    async handler({ fileTitle }) {
-        await readFromFile(fileTitle);
+    async handler({ filetitle }) {
+        await readFromFile(filetitle);
     }
 });
+
+yargs.command({
+    command: "osinfo",
+    description: "write current os info",
+    async handler() {
+        const infoBlock = Object.keys(osInfo).map(key => ({ [key]: osInfo[key] }));
+        await createNewFile(path.resolve(__dirname, "os"), "osInfo.json", infoBlock);
+    }
+})
+
+yargs.command({
+    command: "removefile",
+    builder: {
+        filepath: {
+            type: "string",
+            description: "path to file to be removed",
+            demandOption: true
+        }
+    },
+    description: "delete file",
+    async handler({ filepath }) {
+        await removeFile(path.resolve(__dirname, filepath));
+    }
+})
+
+yargs.command({
+    command: "removedir",
+    builder: {
+        dirpath: {
+            type: "string",
+            description: "path to dir to be removed",
+            demandOption: true
+        }
+    },
+    description: "delete dir",
+    async handler({ dirpath }) {
+        await removeDir(path.resolve(__dirname, dirpath));
+    }
+})
+
+yargs.command({
+    command: "truncfile",
+    builder: {
+        filepath: {
+            type: "string",
+            description: "path to file to be truncated",
+            demandOption: true
+        }
+    },
+    description: "truncate file",
+    async handler({ filepath }) {
+        await truncateFile(path.resolve(__dirname, filepath));
+    }
+})
+
+yargs.command({
+    command: "copy",
+    builder: {
+        src: {
+            type: "string",
+            description: "source of file to be copied from",
+            demandOption: true
+        },
+        dest: {
+            type: "string",
+            description: "destionation of file to be copied into",
+            demandOption: true
+        }
+    },
+    description: "copy file",
+    async handler({ src, dest }) {
+        const sourcePath = path.join(__dirname, src);
+        const destinationPath = path.join(__dirname, dest);
+        await copy(sourcePath, destinationPath);
+    }
+})
+
+yargs.command({
+    command: "rename",
+    builder: {
+        oldpath: {
+            type: "string",
+            description: "old path of a file or dir to be renamed from",
+            demandOption: true
+        },
+        newpath: {
+            type: "string",
+            description: "new path of a file or dir to be renamed for",
+            demandOption: true
+        }
+    },
+    description: "rename a file or dir",
+    async handler({ oldpath, newpath }) {
+        const oldPath = path.resolve(__dirname, oldpath);
+        const newPath = path.resolve(__dirname, newpath);
+        await rename(oldPath, newPath);
+    }
+})
 
 yargs.parse();
